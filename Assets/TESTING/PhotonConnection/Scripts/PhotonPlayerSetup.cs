f1a1dde8b93390f17dfef1dc7796ea2668f25f45
beatdones <beatdonesdesigner@gmail.com>
2022-06-22 09:22:25 +0200

    KDA settings

---
+++ Assets/TESTING/PhotonConnection/Scripts/PhotonPlayerSetup.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotonPlayerSetup : MonoBehaviour
+{
+    public string[] playersNames;
+    public Text[] playerNameTexts;
+    public int[] playerScores;
+    public int scoreTotal;
+    public Text[] playerScoreText;
+    public Transform[] scoreOrder;
+
+
+
+}
